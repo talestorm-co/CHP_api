6c94890ee01ab37d11e56e3416e63aa30a471126
Tarangok <tgntnl@yandex.com>
2020-10-21 10:15:37 +0700

    created class with placeholders

---
+++ src/CHP_api/CHP_adapter.py
@@ -0,0 +1,64 @@
+import requests
+
+
+class CHP_api():
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+    def cancel_bid_ask(self):
+        pass
+
+    def cancel_order(self):
+        pass
+
+    def cancel_portfolio(self):
+        pass
+
+    def cancel_quotes(self):
+        pass
+
+    def cancel_ticks(self):
+        pass
+
+    def connected(self):
+        pass
+
+    def disconnected(self):
+        pass
+
+    def get_bars(self):
+        pass
+
+    def get_my_portfolio_data(self):
+        pass
+
+    def get_trades(self):
+        pass
+
+    def get_portfolio_list(self):
+        pass
+
+    def get_symbols(self):
+        pass
+
+    def is_connected(self):
+        pass
+
+    def listen_bid_asks(self):
+        pass
+
+    def listen_portfolio(self):
+        pass
+
+    def listen_quotes(self):
+        pass
+
+    def listen_ticks(self):
+        pass
+
+    def move_order(self):
+        pass
+
+    def place_order(self):
+        pass
