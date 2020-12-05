36a7bdb0f0941b2bf2d54e168fff1fc4bb5c7001
KrimsN <deni.tim99@yandex.ru>
2020-12-05 14:40:36 +0700

    lowlevel_api updated

--- src/CHP_api/CHP_lowlevel_api.py
+++ src/CHP_api/CHP_lowlevel_api.py
@@ -6,15 +6,35 @@ import typing as t
 
 class Api:
 
-    def __init__(self, host: str, port: t.Union[int, str], ssh=False) -> None:
-
+    def __init__(self, host: str, port: t.Union[int, str] = 5001, ssh: bool = False) -> None:
+        """
+        Initialise Api instance with address to connection remote api.
+        :param host: API host address (IP address or url domain)
+        :param port: port to connect. Default: 5001
+        :param ssh: if ssh is True connection by https, else by http. Default: False
+        """
         if port:
             self._host_url = f"{'https://' if ssh else 'http://'}{host}"
             if port:
                 self._host_url += f":{port}"
 
     def _req_method(self, method_url: str, method_data: t.Optional[t.Dict] = None) -> requests.Response:
+        """
+        Sending post request to <full_host_address>/<method_url> with json context: <method_data>
+        For example:
+            _req_method(
+                method_url='Trade',
+                method_data={
+                    "token":'12345'
+                }
+            )
+            is sending post to http://127.0.0.1:5001/Trade
+            with json {"token":"12345"}
 
+        :param method_url:
+        :param method_data:
+        :return:
+        """
         resp = requests.post(
             f'{self._host_url}/{method_url}',
             json=method_data,
@@ -22,114 +42,140 @@ class Api:
         )
         return resp
 
-    def Connect(self, login, password, token, mode):
+    def Connect(self, login: str, password: str, token: str, mode: int) -> requests.Response:
+        """
+        Sending request to connecting to remote api with your auth data
+
+        :param login: user login into ITI Capital
+        :param password: user password into ITI Capital
+        :param token: user auth token
+        :param mode: mode to connect [0 - demo | 1 - real connection]
+        :return:
+        """
         resp = self._req_method(
-                        'Auth/Connected',
-                        method_data={
-                            'login': login,
-                            'password': password,
-                            'token': token,
-                            'mode': mode,
-                        }
-                    )
-
+            method_url='Auth/Connected',
+            method_data={
+                'login': login,
+                'password': password,
+                'token': token
+                # 'mode': mode, # TODO Узнать что там с модом
+            }
+        )
         return resp
 
-    def GetBars(self, token, symbol, interval, since, count):
-        resp = self._req(
-            sub_url='Instruments/GetBars',
-            data={
+    def GetBars(self, token: str, since: str, interval: int, symbol: str, count: int) -> requests.Response:
+
+        """
+
+        :param token: user auth token
+        :param since: since date like "2020-10-10T10:10:10.000"
+        :param interval: bar interval
+        :param symbol: symbol like "GAZP"
+        :param count: count of bars since "since" parameter
+        :return:
+        """
+        resp = self._req_method(
+            method_url='Instruments/GetBars',
+            method_data={
                 'token': token,
+                "Since": since,
+                "Interval": interval,
                 'symbol': symbol,
-                'interval': interval,
-                'since': since,
                 'count': count,
             }
         )
+
         return resp
 
-    def GetSymbols(self, token):
-        resp = self._req(
-            sub_url='Instruments/GetSymbols',
-            data={
-                'token': token,
-            }
-        )
-        return resp
+    def GetSymbols(self, token: str, ):  # TODO
+        pass
 
-    def GetMyPotfolioData(self, token, mode, portfolio):
-        resp = self._req(
-            sub_url='HistoricalData/GetMyPortfolioData',
-            data={
-                'token': token,
-                'mode': mode,
-                'portfolio': portfolio,
-            }
-        )
-        return resp
+    def GetMyPortfolioData(self, token: str, ) -> requests.Response:  # TODO
+        pass
 
-    def GetPortfolioList(self, token):
-        resp = self._req(
-            sub_url='AccountInformation/GetPortfolioList',
-            data={
-                'token': token,
-            }
-        )
-        return resp
+    def GetPortfolioList(self, token):  # TODO
+        pass
 
-    def GetTrades(self, token, symbol, interval, count):
-        resp = self._req(
-            sub_url='Instruments/GetTrades',
-            data={
-                'token': token,
-                'symbol': symbol,
-                'from': interval,
-                'count': count,
-            }
-        )
-        return resp
+    def GetTrades(self, token, ):  # TODO
+        pass
 
-    def AddTickHistory(self, token):
-        resp = self._req(
-            sub_url='Instruments/AddTickHistory',
-            data={
-                'token': token,
-            }
-        )
-        return resp
+    def AddTickHistory(self, token):  # TODO
+        pass
 
-    def ListenQuotes(self, token, symbol):
-        resp = self._req(
-            sub_url='Instruments/ListenQuotes',
-            data={
+    def ListenQuotes(self, token: str, symbol: str) -> requests.Response:
+        """
+
+        :param token: user auth token
+        :param symbol: symbol like "GAZP"
+        :return:
+        """
+        resp = self._req_method(
+            method_url='Instruments/ListenQuotes',
+            method_data={
                 'token': token,
                 'symbol': symbol,
             }
         )
         return resp
 
-    def UpdateQuotes(self, token):
-        resp = self._req(
-            sub_url='Instruments/UpdateQuote',
-            data={
+    def UpdateQuote(self, token: str) -> requests.Response:
+        """
+
+        :param token: user auth token
+        :return:
+        """
+        resp = self._req_method(
+            method_url='Instruments/UpdateQuote',
+            method_data={
                 'token': token,
             }
         )
         return resp
 
-    def CancelQuotes(self, ):  # Todo
-        pass
+    def CancelQuotes(self, token: str, symbol: str) -> requests.Response:
+        """
 
-    def ListenTicks(self, ):  # Todo
-        pass
+        :param token: user auth token
+        :param symbol: symbol like "GAZP"
+        :return:
+        """
+        resp = self._req_method(
+            method_url='Instruments/CancelQuotes',
+            method_data={
+                "token": token,
+                "symbol": symbol
+            }
+        )
+
+    def ListenTicks(self, token: str, symbol: str) -> requests.Response:  # Todo
+        """
+
+        :param token: user auth token
+        :param symbol: symbol like "GAZP"
+        :return:
+        """
+        resp = self._req_method(
+            method_url='Instruments/ListenTicks',
+            method_data={
+                "token": token,
+                "symbol": symbol
+            }
+        )
+        return resp
 
     def UpdateTicks(self, ):  # Todo
         pass
 
-    def AddTick(self, token):
-        resp = self._req(
-            sub_url='Instruments/AddTick',
-            data={
+    def AddTick(self, token: str) -> requests.Response:
+        """
+
+        :param token: user auth token
+        :return:
+        """
+
+        resp = self._req_method(
+            method_url='Instruments/AddTick',
+            method_data={
                 'token': token,
             }
         )
@@ -138,47 +184,51 @@ class Api:
     def CancelTicks(self, ):  # Todo
         pass
 
-    def ListenBidAsks(self, ):  # Todo
-        pass
+    def ListenBidAsks(self, token: str, symbol: str) -> requests.Response:
+        """
+
+        :param token: user auth token
+        :param symbol: symbol like "GAZP"
+        :return:
+        """
+        resp = self._req_method(
+            method_url='Instruments/ListenBidAsks',
+            method_data={
+                "token": token,
+                "symbol": symbol,
+            }
+        )
+        return resp
 
     def UpdateBidAsks(self, ):  # Todo
         pass
 
-    def CancelBidAsks(self, ):  # Todo
+    def CancelBidAsks(self, token: str, symbol: str) -> requests.Response:
+        """
+
+        :param token: user auth token
+        :param symbol: symbol like "GAZP"
+        :return:
+        """
+        resp = self._req_method(
+            method_url='Instruments/CancelBidAsks',
+            method_data={
+                "token": token,
+                "symbol": symbol
+            }
+        )
+        return resp
+
+    def ListenPortfolio(self, token: str, ):
         pass
 
-    def ListenPortfolio(self, token, portfolio):
-        resp = self._req(
-            sub_url='AccountInformation/ListenPortfolio',
-            data={
-                'token': token,
-                'portfolio': portfolio
-            }
-        )
-        return resp
+    def PlaceOrder(self, token, portfolio, symbol, action, type, validity, price, amount, stop, cookie):  # TODO
+        pass
 
-    def PlaceOrder(self, token, portfolio, symbol, action, type, validity, price, amount, stop, cookie):
-        resp = self._req(
-            sub_url='Order/Place',
-            data={
-                'token': token,
-                'portfolio': portfolio,
-                'symbol': symbol,
-                'action': action,
-                'type': type,
-                'validity': validity,
-                'price': price,
-                'amount': amount,
-                'stop': stop,
-                'cookie': cookie
-            }
-        )
-        return resp
-
-    def MoveOrder(self, token, portfolio, symbol, orderid, targetprice):
-        resp = self._req(
-            sub_url='Order/Move',
-            data={
+    def MoveOrder(self, token, portfolio, symbol, orderid, targetprice):  # TODO
+        resp = self._req_method(
+            method_url='Order/Move',
+            method_data={
                 'token': token,
                 'portfolio': portfolio,
                 'symbol': symbol,
@@ -188,10 +238,10 @@ class Api:
         )
         return resp
 
-    def CancelOrder(self, token, portfolio, symbol, orderid):
-        resp = self._req(
-            sub_url='Order/Cancel',
-            data={
+    def CancelOrder(self, token, portfolio, symbol, orderid):  # TODO
+        resp = self._req_method(
+            method_url='Order/Cancel',
+            method_data={
                 'token': token,
                 'portfolio': portfolio,
                 'symbol': symbol,
@@ -200,63 +250,69 @@ class Api:
         )
         return resp
 
-    def UpdateOrder(self, token):
-        resp = self._req(
-            sub_url='AccountInformation/UpdateOrder',
-            data={
+    def UpdateOrder(self, token):  # TODO
+        resp = self._req_method(
+            method_url='AccountInformation/UpdateOrder',
+            method_data={
                 'token': token,
             }
         )
         return resp
 
-    def UpdatePosition(self, token):
-        resp = self._req(
-            sub_url='AccountInformation/UpdatePosition',
-            data={
+    def UpdatePosition(self, token):  # TODO
+        resp = self._req_method(
+            method_url='AccountInformation/UpdatePosition',
+            method_data={
                 'token': token,
             }
         )
         return resp
 
-    def AddTrade(self, token):
-        resp = self._req(
-            sub_url='AccountInformation/AddTrade',
-            data={
+    def AddTrade(self, token):  # TODO
+        resp = self._req_method(
+            method_url='AccountInformation/AddTrade',
+            method_data={
                 'token': token,
             }
         )
         return resp
 
-    def SetPortfolio(self, token):
-        resp = self._req(
-            sub_url='AccountInformation/SetPortfolio',
-            data={
+    def SetPortfolio(self, token):  # TODO
+        resp = self._req_method(
+            method_url='AccountInformation/SetPortfolio',
+            method_data={
                 'token': token,
             }
         )
         return resp
 
-    def CancelPortfolio(self, token, portfolio):
-        resp = self._req(
-            sub_url='AccountInformation/CancelPortfolio',
-            data={
+    def CancelPortfolio(self, token, portfolio):  # TODO
+        resp = self._req_method(
+            method_url='AccountInformation/CancelPortfolio',
+            method_data={
                 'token': token,
                 'portfolio': portfolio
             }
         )
         return resp
 
-    def Disconnected(self, login, password, token):
-        resp = self._req(
-            sub_url='AccountInformation/CancelPortfolio',
-            data={
+    def Disconnected(self, login: str, password: str, token: str) -> requests.Response:
+        """
+        Sending request to disconnect user from API, and close thread connection
+        :param login: user login into ITI Capital
+        :param password: user password into ITI Capital
+        :param token: user auth token
+        :return:
+        """
+        resp = self._req_method(
+            method_url='Auth/Disconnected',
+            method_data={
                 'login': login,
                 'password': password,
                 'token': token
             }
         )
         return resp
-
 
 
 if __name__ == '__main__':
