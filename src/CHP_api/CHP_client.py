9246530616d6295e25a3ad2032be3dd8193c864c
KrimsN <deni.tim99@yandex.ru>
2020-12-05 15:55:39 +0700

    Updated client

--- src/CHP_api/CHP_client.py
+++ src/CHP_api/CHP_client.py
@@ -1,7 +1,11 @@
-from CHP_api.CHP_lowlevel_api import Api
 import typing as t
+from json import loads
 
+from CHP_api.CHP_lowlevel_api import Api
 from CHP_api.utils.Meta import SmartClientSingleton
+from CHP_api.CHPExceptions import (
+    ApiConnectionError
+)
 
 
 class ChpClient(metaclass=SmartClientSingleton):
@@ -10,9 +14,9 @@ class ChpClient(metaclass=SmartClientSingleton):
             self,
             host: str,
             port: t.Union[str, int],
-            login: str = '',
-            password: str = '',
-            token: str = '',
+            login: str,
+            password: str,
+            token: str,
             mode: int = 0
     ) -> None:
 
@@ -23,20 +27,67 @@ class ChpClient(metaclass=SmartClientSingleton):
             For example: '127.0.0.1'
         :param port: port to connect to api server.
             For example: 5001
-        :param login: your IT Capital account login.
-        :param password: your IT Capital account password
+        :param login: your ITI Capital account login.
+        :param password: your ITI Capital account password
         :param token: your access token
         :param mode: connect mode [0 - demo | 1 - production]
         """
 
-        # self._api = Api(host, port)  # Todo
+        self._api = Api(host=host, port=port, ssh=False)
 
-        self._token = token
+        connect_resp = self._api.Connect(login=login, password=password, token=token, mode=mode)
+        connect_resp = loads(connect_resp.text)[0]
+        if not connect_resp['result']:
+            raise ApiConnectionError(connect_resp['reason'])
+
+        self._token: str = token
+        self._login: str = login
+        self._password: str = password
 
     @property
     def token(self) -> str:
         return self._token
 
+    @property
+    def login(self) -> str:
+        return self._login
+
+    @property
+    def password(self) -> str:
+        return self._password
+
+    def GetBars(self, since: str, interval: int, symbol: str, count: int):
+        """
+
+        :param since:
+        :param interval:
+        :param symbol:
+        :param count:
+        :return:
+        """
+        resp = self._api.GetBars(token=self._token, since=since, interval=interval,
+                                 symbol=symbol, count=count)
+
+    def GetSymbols(self):  # TODO
+        pass
+
+    def GetMyPortfolioData(self):  # TODO
+        pass
+
+    def GetPortfolioList(self):  # TODO
+        pass
+
+    def GetTrades(self):  # TODO
+        pass
+
+    def AddTickHistory(self):  # TODO
+        pass
+
     def __del__(self):
-        # self._api.disconnect(token=self._token) # Todo
+        disconnect_resp = self._api.Disconnected(login=self._login, password=self._password,
+                                                 token=self._token)
+        disconnect_resp = loads(disconnect_resp.text)[0]
+        if not disconnect_resp['result']:
+            raise ApiConnectionError('Ошибка отключения от api, Сообзите о проблеме разработчикам')
+
         print(f"Client with token {self._token} deleted")
