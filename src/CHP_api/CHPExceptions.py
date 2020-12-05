18c27e2b04d4ec56dd6a3e897a2d90fd7db9f8c4
KrimsN <deni.tim99@yandex.ru>
2020-12-05 15:55:27 +0700

    Added ConnectionError

--- src/CHP_api/CHPExceptions.py
+++ src/CHP_api/CHPExceptions.py
@@ -1,4 +1,11 @@
-
+# CHP_adapter #############
 class LoginException(AttributeError):
+    def __init__(self, string_message: str):
+        self.txt = string_message
+###########################
+
+
+# ChpClient ###############
+class ApiConnectionError(RuntimeError):
     def __init__(self, string_message: str):
         self.txt = string_message
