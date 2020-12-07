2244769f71bdcb5bd5001fa074291d477c12fc98
KrimsN <deni.tim99@yandex.ru>
2020-12-07 19:30:00 +0700

    client update

--- src/CHP_api/CHP_client.py
+++ src/CHP_api/CHP_client.py
@@ -35,7 +35,7 @@ class ChpClient(metaclass=SmartClientSingleton):
 
         self._api = Api(host=host, port=port, ssh=False)
 
-        connect_resp = self._api.Connect(login=login, password=password, token=token, mode=mode)
+        connect_resp = self._api.Connected(login=login, password=password, token=token, mode=mode)
         connect_resp = jsonify(connect_resp.text)
         if not connect_resp['result']:
             raise ApiConnectionError(connect_resp['reason'], data=connect_resp)
@@ -44,23 +44,33 @@ class ChpClient(metaclass=SmartClientSingleton):
         self._login: str = login
         self._password: str = password
 
-        self.__listening_quotes: t.List = []
+        self.__listening_quotes: t.List[str] = []
+        self.__listening_ticks: t.List[str] = []
+        self.__listening_bid_ask: t.List[str] = []
 
     @property
-    def token(self) -> str:
+    def Token(self) -> str:
         return self._token
 
     @property
-    def login(self) -> str:
+    def Login(self) -> str:
         return self._login
 
     @property
-    def password(self) -> str:
+    def Password(self) -> str:
         return self._password
 
     @property
     def listening_quotes(self) -> t.List[str]:
         return [*self.__listening_quotes]
+
+    @property
+    def listening_ticks(self) -> t.List[str]:
+        return [*self.__listening_ticks]
+
+    @property
+    def listening_bid_ask(self) -> t.List[str]:
+        return [*self.__listening_bid_ask]
 
     @staticmethod
     def _check_response(resp):
@@ -100,7 +110,13 @@ class ChpClient(metaclass=SmartClientSingleton):
         return resp['data']
 
     def GetTrades(self, since: str, symbol: str, count: int):  # TODO
+        """
 
+        :param since:
+        :param symbol:
+        :param count:
+        :return:
+        """
         resp = self._api.GetTrades(
             token=self._token,
             since=since,
@@ -114,7 +130,7 @@ class ChpClient(metaclass=SmartClientSingleton):
 
     def ListenQuotes(self, symbols: t.List[str]) -> t.Dict[str, bool]:
         """
-
+        Listening on the quotes by symbols
         :param symbols: list of symbols  like ['GAZP'] or ['GAZP', 'SBER']
         :return:
         """
@@ -136,7 +152,7 @@ class ChpClient(metaclass=SmartClientSingleton):
 
     def UpdateQuote(self):
         """
-        
+        get quotes for all listening symbols
         :return:
         """
         resp = self._api.UpdateQuote(token=self._token)
@@ -148,12 +164,13 @@ class ChpClient(metaclass=SmartClientSingleton):
 
     def CancelQuotes(self, symbols: t.Optional[t.List[str]] = None) -> t.Dict[str, bool]:
         """
-
+        Unsubscribe from the specified quotes (if nothing is passed, unsubscribe from all quotes)
         :param symbols: [Optional] if None Cancel all listening Quotes. else list of symbols  like ['GAZP']
         or ['GAZP', 'SBER']
         :return:
         """
-        if symbols is None:
+        if symbols is None or\
+                not symbols:
             symbols = [*self.__listening_quotes]
         elif not isinstance(symbols, list):
             raise TypeError('the symbols must be list instance')
@@ -165,24 +182,110 @@ class ChpClient(metaclass=SmartClientSingleton):
 
             results[symbol] = resp['result']
             if resp['result']:
-                self.__listening_quotes.remove()
+                self.__listening_quotes.remove(symbol)
 
         return results
 
+    def ListenTicks(self, symbols: t.List[str]):
+        """
+        Listening on the ticks by symbols
+        :param symbols: list of symbols  like ['GAZP'] or ['GAZP', 'SBER']
+        :return:
+        """
+        if not isinstance(symbols, list):
+            raise TypeError('the symbols must be list instance')
 
+        results = {}
+        for symbol in symbols:
+            if symbol not in self.__listening_ticks:
+                resp = self._api.ListenTicks(token=self._token, symbol=symbol)
+                resp = jsonify(resp.text)
 
+                results[symbol] = resp['result']
 
+                if resp['result']:
+                    self.__listening_ticks.append(symbol)
 
+        return results
 
+    def UpdateTicks(self):
+        """
+        get ticks for all listening symbols
+        :return:
+        """
+        resp = self._api.AddTick(token=self._token)
+        resp = jsonify(resp.text)
 
+        self._check_response(resp)
 
+        return resp['data']
 
+    def CancelTicks(self, symbols: t.Optional[t.List[str]] = None) -> t.Dict[str, bool]:
+        """
+        Unsubscribe from the specified ticks (if nothing is passed, unsubscribe from all quotes)
+        :param symbols: [Optional] if None Cancel all listening ticks. else list of symbols  like ['GAZP']
+        or ['GAZP', 'SBER']
+        :return:
+        """
+        if symbols is None or\
+                not symbols:
+            symbols = [*self.__listening_ticks]
+        elif not isinstance(symbols, list):
+            raise TypeError('the symbols must be list instance')
 
+        results = {}
+        for symbol in symbols:
+            resp = self._api.CancelTicks(token=self._token, symbol=symbol)
+            resp = jsonify(resp.text)
 
+            results[symbol] = resp['result']
+            if resp['result']:
+                self.__listening_ticks.remove(symbol)
 
+        return results
 
+    def ListenBidAsks(self, symbols):
 
+        if not isinstance(symbols, list):
+            raise TypeError('the symbols must be list instance')
 
+        results = {}
+        for symbol in symbols:
+            if symbol not in self.__listening_bid_ask:
+                resp = self._api.ListenBidAsks(token=self._token, symbol=symbol)
+                resp = jsonify(resp.text)
+
+                results[symbol] = resp['result']
+
+                if resp['result']:
+                    self.__listening_bid_ask.append(symbol)
+
+        return results
+
+    def UpdateBidAsks(self):
+        resp = self._api.UpdateBidAsk(token=self._token)
+        resp = jsonify(resp.text)
+        self._check_response(resp)
+        return resp['data']
+
+    def CancelBidAsks(self, symbols: t.Optional[t.List[str]] = None):
+
+        if symbols is None or\
+                not symbols:
+            symbols = [*self.__listening_bid_ask]
+
+        elif not isinstance(symbols, list):
+            raise TypeError('the symbols must be list instance')
+
+        results = {}
+        for symbol in symbols:
+            resp = self._api.CancelBidAsks(token=self._token, symbol=symbol)
+            resp = jsonify(resp.text)
+            results[symbol] = resp['result']
+            if resp['result']:
+                self.__listening_bid_ask.remove(symbol)
+
+        return results
 
     def __del__(self):
         disconnect_resp = self._api.Disconnected(
