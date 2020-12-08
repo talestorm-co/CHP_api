ad39d32620a1a45f91238db1642face8162ef274
KrimsN <deni.tim99@yandex.ru>
2020-12-08 21:17:22 +0700

    new version v1.0.1

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(name='CHP_api',
-      version='1.0.0',
+      version='1.0.1',
       description='Simple python adaptor for CHP_REST',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='https://github.com/talestorm-com/CHP_api',
-      author='talestorm',
+      author='KrimsN',
       author_email='dr.krimsn@gmail.com',
       package_dir={'': 'src'},
       packages=find_packages(where='src'),
