1db4626c82cdd3d0a499c399e2330eab8401ef77
KrimsN <deni.tim99@yandex.ru>
2020-12-08 22:18:16 +0700

    asd

--- src/CHP_api/__init__.py
+++ src/CHP_api/__init__.py
@@ -1,3 +1,9 @@
 from CHP_api.CHP_adapter import CHP_api
 
 from CHP_api.CHP_client import ChpClient
+
+from CHP_api.CHPExceptions import (
+    ChpError,
+    ApiConnectionError,
+    ApiRequestException
+)
